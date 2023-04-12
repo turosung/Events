aeaf770c3d689051fd8d8292ca1aab2589576e66
Nuhu Sulemana <turosung@gmail.com>
2023-04-12 14:52:35 +0100

    Created a simple Ui to change label

--- Events/ViewController.swift
+++ Events/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var toggleSwitch: UISwitch!
+    @IBOutlet weak var titleLable: UILabel!
+    @IBOutlet weak var nameField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+
+    @IBAction func submitButtonClicked(_ sender: UIButton) {
+        let name = nameField.text ?? ""
+        titleLable.text = "Marhaba, \(name)"
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        nameField.resignFirstResponder()
+    }
+    
+    @IBAction func switchToggled(_ sender: UISwitch) {
+        if sender.isOn {
+            self.view.backgroundColor = .yellow
+        } else {
+            self.view.backgroundColor = .darkGray
+        }
+    }
 
 }
 
